213715f298ddcaf941ff150dac198a2cb97aa991
TMarcellB <tulakm@kkszki.hu>
2024-06-06 11:07:54 +0200

    css jobblett valamennyire

--- ma/js.js
+++ ma/js.js
@@ -7,10 +7,10 @@ this.window.onload= function(){
         for (const adat of valasz2) {
             console.log(adat)
             document.getElementById("adatok").innerHTML += 
-            ` <div class="card" style="width: 18rem;">
+            ` <div class="card " style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">${adat.name}</h5>
-              <a  target="blank" href="www.${adat.hostname}">Weblap</a>
+              <a  target="blank" href="http://www.${adat.hostname}">Weblap</a>
               <p>${adat.location}</p>
               <p>${adat.price} Ft</p>
               <p>Minimum éjszakák száma ${adat.minimum_nights}</p> 
