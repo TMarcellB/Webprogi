59f6a9c18d6fad9b4a8eacc2b9ae52ea4d7dce22
TMarcellB <tulakm@kkszki.hu>
2024-06-06 11:36:59 +0200

    Módositás

--- ma/js.js
+++ ma/js.js
@@ -15,6 +15,7 @@ this.window.onload= function(){
               <p>${adat.price} Ft</p>
               <p>Minimum éjszakák száma ${adat.minimum_nights}</p> 
               <button id="torles" onclick="Torles(${adat.id})" class="btn btn-primary"> Törlés</button>
+              <button class="btn"  alt="Módosítás" onclick="FetchPUTatvezet(${adat.id})">Módos</button>
             </div>`;
         }
     })}
@@ -48,4 +49,55 @@ this.window.onload= function(){
         .then(function(valasz) {
             return valasz.json()
         })
+    }
+    function FetchPUTatvezet(azon) {
+        localStorage.setItem("azon", azon);
+        console.log(localStorage.getItem("azon"));
+    
+        window.open("felvitel.html","_self")
+    }
+    
+    if((localStorage.getItem("azon") > -1) && window.location.href.includes("felvitel.html"))
+    {
+        fetch("http://nodejs.sulla.hu/data/"+localStorage.getItem("azon"))
+        .then(function(valasz){
+        return valasz.json();
+        })
+        .then(function(valasz2){
+        console.log(valasz2);
+        document.getElementById("name").value = valasz2.name;
+        document.getElementById("hostname").value = valasz2.hostname;
+        document.getElementById("location").value = valasz2.location;
+        document.getElementById("price").value = valasz2.price;
+        document.getElementById("minimum").value = valasz2.minimum_nights;
+        })
+        document.getElementById("Feltoltes").innerText="PUT";
+        document.getElementById("Feltoltes").setAttribute('onclick','FetchPUT()')
+    }
+    
+    function FetchPUT() {
+        let adatok = JSON.stringify({
+            name: document.getElementById("name").value,
+            hostname: document.getElementById("hostname").value,
+            location: document.getElementById("location").value,
+            price: document.getElementById("price").value,
+            minimum_nights: document.getElementById("minimum").value
+        })
+        fetch("http://nodejs.sulla.hu/data/"+localStorage.getItem("azon"),
+        {
+            method: "PUT",
+            headers: { 'Content-Type': 'application/json' },
+            body: adatok,
+        })
+        .then(
+            alert("Sikeres módosítás!"),
+            localStorage.setItem("azon", -1),
+            console.log(localStorage.getItem("azon")),
+            document.getElementById("name").value = "",
+            document.getElementById("hostname").value = "",
+            document.getElementById("location").value = "",
+            document.getElementById("price").value = "",
+            document.getElementById("minimum").value = "",
+            location.reload()
+        )
     }
