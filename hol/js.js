7f8edb0aeb14f3f280f0d79833f7bfc14614d384
TMarcellB <tulakm@kkszki.hu>
2024-09-24 12:10:00 +0200

    naonjó

---
+++ hol/js.js
@@ -0,0 +1,17 @@
+const hn=1;
+let tomb=[0,1,2,3,4,5];
+console.log(tomb[0])
+console.log(tomb[1])
+console.log(tomb[2])
+console.log(tomb[3])
+console.log(tomb.length)
+for (let index = 0; index < tomb.length; index++) {
+console.log(tomb[index])
+}
+let f={
+   
+    as:5,
+    ap:6,
+    asd:7
+}
+console.log(f.asd);
